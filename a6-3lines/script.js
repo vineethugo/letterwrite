7421d3a3a5adf4e83586f966b7c09ed3526cf651
vineethugo <vineethugo@users.noreply.github.com>
2024-08-21 21:02:18 -0500

    Update script.js

--- a6-3lines/script.js
+++ a6-3lines/script.js
@@ -46,8 +46,8 @@ function drawLineWithCircles(x1, y1, x2, y2, color, delay, text, textX, textY) {
 // Coordinates for the letter "A"
 const lines = [
     { x1: 100, y1: 300, x2: 200, y2: 100, color: 'red', text: '1', textX: 90, textY: 250 },  // Left of the red line
-    { x1: 200, y1: 100, x2: 300, y2: 300, color: 'green', text: '2', textX: 310, textY: 200 }, // Right of the green line
-    { x1: 150, y1: 200, x2: 250, y2: 200, color: 'blue', text: '3', textX: 190, textY: 220 }  // Below the blue line
+    { x1: 200, y1: 100, x2: 300, y2: 300, color: 'green', text: '2', textX: 310, textY: 250 }, // Right of the green line (aligned with number 1)
+    { x1: 150, y1: 200, x2: 250, y2: 200, color: 'blue', text: '3', textX: 190, textY: 260 }  // Below the blue line (moved further down)
 ];
 
 lines.forEach((line, index) => {
